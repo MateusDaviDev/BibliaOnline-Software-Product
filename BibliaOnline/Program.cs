f1831697364b87513c8842c429c6178a257f28af
MateusDaviDev <mateusdavi.silva@hotmail.com>
2024-10-11 22:37:40 -0300

    AC02

--- BibliaOnline/Program.cs
+++ BibliaOnline/Program.cs
@@ -1,6 +1,14 @@
 using BibliaOnline.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using BibliaOnline.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DBContextSampleConnection") ?? throw new InvalidOperationException("Connection string 'DBContextSampleConnection' not found.");
+
+builder.Services.AddDbContext<DBContextSample>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<SampleUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<DBContextSample>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -27,5 +35,5 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapRazorPages();
 app.Run();
